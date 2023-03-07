ee3533efbd2b31fae88d4d98029071ded81ba71c
Wulf2k <wulf2k@gmail.com>
2023-03-07 01:31:32 -0600

    Fixed an off-by-one issue

--- GoWSaveEdit/GoWFuncs.vb
+++ GoWSaveEdit/GoWFuncs.vb
@@ -10,58 +10,57 @@ Public Class GoWFuncs
     Public Shared SecureID() As Byte = {&H82, &H21, &H42, &HD2, &H27, &H74, &H97, &H6, &H62, &H25, &H46, &HE6, &HE7, &H20, &H6, &H27}
 
     Shared Function RInt32(ByRef bytes() As Byte, start As Int32) As Int32
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         Array.Copy(bytes, start, ba, 0, 4)
         If bigendian Then Array.Reverse(ba)
 
-        Return BitConverter.ToInt32(ba, 1)
+        Return BitConverter.ToInt32(ba, 0)
     End Function
     Shared Function RSingle(ByRef bytes() As Byte, start As Int32) As Single
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         Array.Copy(bytes, start, ba, 0, 4)
         If bigendian Then Array.Reverse(ba)
 
-        REM TODO: ...Why is this not acting like it's zero-indexed?
-        Return BitConverter.ToSingle(ba, 1)
+        Return BitConverter.ToSingle(ba, 0)
     End Function
     Shared Function RUInt32(ByRef bytes() As Byte, start As Int32) As UInt32
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         Array.Copy(bytes, start, ba, 0, 4)
         If bigendian Then Array.Reverse(ba)
 
-        Return BitConverter.ToUInt32(ba, 1)
+        Return BitConverter.ToUInt32(ba, 0)
     End Function
 
     Shared Sub WInt16(ByRef bytes() As Byte, start As Int32, val As Int16)
-        Dim ba(2) As Byte
+        Dim ba(1) As Byte
         ba = BitConverter.GetBytes(val)
         If bigendian Then Array.Reverse(ba)
 
         Array.Copy(ba, 0, bytes, start, 2)
     End Sub
     Shared Sub WInt32(ByRef bytes() As Byte, start As Int32, val As Int32)
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         ba = BitConverter.GetBytes(val)
         If bigendian Then Array.Reverse(ba)
 
         Array.Copy(ba, 0, bytes, start, 4)
     End Sub
     Shared Sub WSingle(ByRef bytes() As Byte, start As Int32, val As Single)
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         ba = BitConverter.GetBytes(val)
         If bigendian Then Array.Reverse(ba)
 
         Array.Copy(ba, 0, bytes, start, 4)
     End Sub
     Shared Sub WUInt16(ByRef bytes() As Byte, start As Int32, val As UInt16)
-        Dim ba(2) As Byte
+        Dim ba(1) As Byte
         ba = BitConverter.GetBytes(val)
         If bigendian Then Array.Reverse(ba)
 
         Array.Copy(ba, 0, bytes, start, 2)
     End Sub
     Shared Sub WUInt32(ByRef bytes() As Byte, start As Int32, val As UInt32)
-        Dim ba(4) As Byte
+        Dim ba(3) As Byte
         ba = BitConverter.GetBytes(val)
         If bigendian Then Array.Reverse(ba)
 
